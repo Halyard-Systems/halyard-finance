632d599978d3c672416c3d86bc9fea4080fca04b
Dave Miner <wtrispsn@gmail.com>
2025-08-23 14:22:40 -0400

    add events to MockERC20

--- src/mocks/MockERC20.sol
+++ src/mocks/MockERC20.sol
@@ -7,6 +7,11 @@ contract MockERC20 {
     uint8 public decimals;
     string public name;
     string public symbol;
+    uint256 public totalSupply;
+
+    // Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     constructor(string memory _name, string memory _symbol, uint8 _decimals) {
         name = _name;
@@ -15,27 +20,41 @@ contract MockERC20 {
     }
 
     function mint(address to, uint256 amount) external {
+        require(to != address(0), "ERC20: mint to the zero address");
         balanceOf[to] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), to, amount);
     }
 
     function transfer(address to, uint256 amount) external returns (bool) {
-        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(balanceOf[msg.sender] >= amount, "ERC20: transfer amount exceeds balance");
+        
         balanceOf[msg.sender] -= amount;
         balanceOf[to] += amount;
+        emit Transfer(msg.sender, to, amount);
         return true;
     }
 
     function transferFrom(address from, address to, uint256 amount) external returns (bool) {
-        require(balanceOf[from] >= amount, "USDT transfer failed");
-        require(allowance[from][msg.sender] >= amount, "USDT transfer failed");
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(balanceOf[from] >= amount, "ERC20: transfer amount exceeds balance");
+        require(allowance[from][msg.sender] >= amount, "ERC20: insufficient allowance");
+        
         balanceOf[from] -= amount;
         balanceOf[to] += amount;
         allowance[from][msg.sender] -= amount;
+        
+        emit Transfer(from, to, amount);
         return true;
     }
 
     function approve(address spender, uint256 amount) external returns (bool) {
+        require(spender != address(0), "ERC20: approve to the zero address");
+        
         allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
         return true;
     }
 }
