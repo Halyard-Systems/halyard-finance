a870b8f23a3549cde04528dc966a19f32c5d1df7
Dave Miner <wtrispsn@gmail.com>
2026-01-18 22:03:26 -0500

    base integration test

--- test/integration/BaseIntegrationTest.t.sol
+++ test/integration/BaseIntegrationTest.t.sol
@@ -3,16 +3,20 @@ pragma solidity ^0.8.23;
 
 import {Test, console} from "lib/forge-std/src/Test.sol";
 
-import {AssetRegistry} from "../../../src/hub/AssetRegistry.sol";
-import {DebtManager} from "../../../src/hub/DebtManager.sol";
-import {HubAccessManager} from "../../../src/hub/HubAccessManager.sol";
-import {HubController} from "../../../src/hub/HubController.sol";
-import {LiquidationEngine} from "../../../src/hub/LiquidationEngine.sol";
-import {PositionBook} from "../../../src/hub/PositionBook.sol";
-import {RiskEngine} from "../../../src/hub/RiskEngine.sol";
-import {MockERC20} from "../../../src/mocks/MockERC20.sol";
+import {HubAccessManager} from "../../src/hub/HubAccessManager.sol";
+import {HubController} from "../../src/hub/HubController.sol";
+import {AssetRegistry} from "../../src/hub/AssetRegistry.sol";
+import {PositionBook} from "../../src/hub/PositionBook.sol";
+import {RiskEngine} from "../../src/hub/RiskEngine.sol";
+import {LiquidationEngine} from "../../src/hub/LiquidationEngine.sol";
+import {DebtManager} from "../../src/hub/DebtManager.sol";
 
-contract BaseHubTest is Test {
+import {CollateralVault} from "../../src/spoke/CollateralVault.sol";
+import {LiquidityVault} from "../../src/spoke/LiquidityVault.sol";
+import {SpokeController} from "../../src/spoke/SpokeController.sol";
+
+contract BaseIntegrationTest is Test {
+    // Hub contracts
     HubAccessManager public hubAccessManager;
     HubController public hubController;
     AssetRegistry public assetRegistry;
@@ -21,58 +25,102 @@ contract BaseHubTest is Test {
     LiquidationEngine public liquidationEngine;
     DebtManager public debtManager;
 
+    // Spoke contracts
+    CollateralVault public collateralVault;
+    LiquidityVault public liquidityVault;
+    SpokeController public spokeController;
+
+    // Test accounts
     address public alice = address(0x1);
     address public bob = address(0x2);
     address public charlie = address(0x3);
-    address public mockLzEndpoint = makeAddr("lzEndpoint");
+    address public david = address(0x4);
+    address public admin = address(0x5);
 
-    address public tempDebtManager = makeAddr("debtManager");
+    address public mockLzEndpoint = makeAddr("lzEndpoint");
     address public mockOracle = makeAddr("oracle");
+    // TODO: replace with a router implementation
     address public router = address(this);
 
     function setUp() public virtual {
-        // Deploy HubAccessManager
-        hubAccessManager = new HubAccessManager(address(this));
-
-        // Deploy HubController
         // Put bytecode at mock addresses so calls don't fail with "non-contract address"
         vm.etch(mockLzEndpoint, hex"00");
-        //vm.etch(mockPyth, hex"00");
 
         // Mock LZ endpoint setDelegate (called in OAppCore constructor)
         vm.mockCall(mockLzEndpoint, abi.encodeWithSignature("setDelegate(address)"), abi.encode());
 
-        hubController = new HubController(address(this), mockLzEndpoint);
+        vm.startPrank(admin);
 
-        // Deploy AssetRegistry
+        // Deploy Hub contracts
+        hubAccessManager = new HubAccessManager(admin);
+        hubController = new HubController(admin, mockLzEndpoint);
         assetRegistry = new AssetRegistry(address(hubAccessManager));
         _setupDefaultAssets(assetRegistry);
 
-        // Deploy PositionBook
         positionBook = new PositionBook(address(hubAccessManager));
-
-        // Deploy RiskEngine
-        riskEngine = new RiskEngine(address(hubAccessManager));
-        riskEngine.setDependencies(
-            address(positionBook),
-            address(tempDebtManager), // or mock address if DebtManager not deployed
-            address(assetRegistry),
-            mockOracle
-        );
-
-        liquidationEngine = new LiquidationEngine(address(hubAccessManager));
-        _setupLiquidationEngine(liquidationEngine);
 
         debtManager = new DebtManager(address(hubAccessManager), address(assetRegistry));
         debtManager.setAssetRegistry(address(assetRegistry));
 
+        riskEngine = new RiskEngine(address(hubAccessManager));
+        riskEngine.setDependencies(address(positionBook), address(debtManager), address(assetRegistry), mockOracle);
+
+        liquidationEngine = new LiquidationEngine(address(hubAccessManager));
+        _setupLiquidationEngine(liquidationEngine);
+
         _setupPermissions(hubAccessManager);
+
+        // Deploy Spoke contracts
+        spokeController = new SpokeController(admin, mockLzEndpoint);
+        collateralVault = new CollateralVault(admin, address(spokeController));
+        liquidityVault = new LiquidityVault(admin, address(spokeController));
+
+        // Configure spokeController
+        spokeController.setCollateralVault(address(collateralVault));
+        spokeController.setLiquidityVault(address(liquidityVault));
+
+        address canonicalToken = makeAddr("canonical_token");
+        address spokeToken = makeAddr("spoke_token");
+        uint32 hubEid = 1;
+        uint32 spokeEid = 10;
+
+        spokeController.setCollateralVault(address(collateralVault));
+        spokeController.setLiquidityVault(address(liquidityVault));
+        spokeController.configureHub(hubEid, bytes32("test_hub"));
+        spokeController.configureSpokeEid(spokeEid);
+        spokeController.setTokenMapping(canonicalToken, spokeToken);
+        spokeController.setPeer(hubEid, bytes32("test_hub"));
+
+        vm.stopPrank();
     }
 
     function buildFunctionSelector(bytes4 selector) internal pure returns (bytes4[] memory) {
         bytes4[] memory addfuncs = new bytes4[](1);
         addfuncs[0] = selector;
         return addfuncs;
+    }
+
+    function _setupLiquidationEngine(LiquidationEngine engine) internal {
+        engine.setCollateralConfig(
+            1,
+            address(0x123),
+            LiquidationEngine.CollateralConfig({
+                isSupported: true, ltvBps: 8000, liqThresholdBps: 8500, liqBonusBps: 500, decimals: 18, supplyCap: 0
+            })
+        );
+        engine.setCollateralConfig(
+            2,
+            address(0x124),
+            LiquidationEngine.CollateralConfig({
+                isSupported: true, ltvBps: 8000, liqThresholdBps: 8500, liqBonusBps: 500, decimals: 18, supplyCap: 0
+            })
+        );
+        engine.setDebtConfig(
+            address(0x123), LiquidationEngine.DebtConfig({isSupported: true, decimals: 18, borrowCap: 0})
+        );
+        engine.setDebtConfig(
+            address(0x124), LiquidationEngine.DebtConfig({isSupported: true, decimals: 18, borrowCap: 0})
+        );
     }
 
     function _setupDefaultAssets(AssetRegistry registry) internal {
@@ -102,29 +150,6 @@ contract BaseHubTest is Test {
 
         registry.setBorrowRateApr(1, address(0x123), 1000);
         registry.setBorrowRateApr(2, address(0x124), 500);
-    }
-
-    function _setupLiquidationEngine(LiquidationEngine engine) internal {
-        engine.setCollateralConfig(
-            1,
-            address(0x123),
-            LiquidationEngine.CollateralConfig({
-                isSupported: true, ltvBps: 8000, liqThresholdBps: 8500, liqBonusBps: 500, decimals: 18, supplyCap: 0
-            })
-        );
-        engine.setCollateralConfig(
-            2,
-            address(0x124),
-            LiquidationEngine.CollateralConfig({
-                isSupported: true, ltvBps: 8000, liqThresholdBps: 8500, liqBonusBps: 500, decimals: 18, supplyCap: 0
-            })
-        );
-        engine.setDebtConfig(
-            address(0x123), LiquidationEngine.DebtConfig({isSupported: true, decimals: 18, borrowCap: 0})
-        );
-        engine.setDebtConfig(
-            address(0x124), LiquidationEngine.DebtConfig({isSupported: true, decimals: 18, borrowCap: 0})
-        );
     }
 
     function _setupPermissions(HubAccessManager accessManager) internal {
@@ -158,6 +183,7 @@ contract BaseHubTest is Test {
             buildFunctionSelector(positionBook.finalizePendingWithdraw.selector),
             accessManager.ROLE_HUB_CONTROLLER()
         );
+        // TODO: should be router? verify that router calls validateAndCreateBorrow and createPendingBorrow
         accessManager.setTargetFunctionRole(
             address(positionBook),
             buildFunctionSelector(positionBook.createPendingBorrow.selector),
---
+++ test/integration/Deposit.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {BaseIntegrationTest} from "./BaseIntegrationTest.t.sol";
+
+contract DepositTest is BaseIntegrationTest {
+    function test_DepositSuccess() public {
+        // TODO: test deposit
+    }
+}
